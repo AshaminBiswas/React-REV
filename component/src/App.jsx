6ea960b9ce113600de5fa140223c2ecec3eddee1
Ashamin Biswas <ashaminbiswas1@gmail.com>
2025-10-30 23:01:46 +0530

    feat: initialize React component project with Vite
    - Added package.json with dependencies for React, Vite, and ESLint.
    - Included Vite logo SVG in the public directory.
    - Created App.css for styling the application.
    - Implemented main App component with a Hello component.
    - Added React logo SVG in assets.
    - Developed Hello component to display a greeting message.
    - Set up global styles in index.css for consistent styling.
    - Configured main entry point in main.jsx to render the App component.
    - Created Vite configuration file for project setup.

---
+++ component/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Hello from './components/Hello'
+
+function App() {
+
+  
+
+  return (
+    <div>
+      <h1>This Is Components </h1>
+      <Hello/>
+    </div>
+  )
+}
+
+export default App
---
+++ component/src/components/Hello.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Hello() {
+  return (
+    <div>
+      <h1>Hii, I am Hello Component...</h1>
+    </div>
+  )
+}
+
+export default Hello
