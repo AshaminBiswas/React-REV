5414107a0e5d3c4de3fb816857612f509d15246a
Ashamin Biswas <ashaminbiswas1@gmail.com>
2025-10-30 23:19:03 +0530

    feat: add Hello component with styled heading

--- component/src/components/Hello.jsx
+++ component/src/components/Hello.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import style from "../components/Hello.module.css"
 function Hello() {
   return (
     <div>
-      <h1>Hii, I am Hello Component...</h1>
+      <h1 className={style.heading}>Hii, I am Hello Component...</h1>
     </div>
   )
 }
