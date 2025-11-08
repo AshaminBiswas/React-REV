8e181d47126579adf7697877934789f06baa6b15
Ashamin Biswas <ashaminbiswas1@gmail.com>
2025-11-08 21:48:08 +0530

    feat: add hobbies prop to Hello component for displaying user hobbies

--- props/src/App.jsx
+++ props/src/App.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import Hello from './components/Hello'
 function App() {
 
+  const hobbies = ["Coding", "Cricket", "BGMI"]
 
   return (
     <div>
-      <Hello name="Ashamin"  age={25} city="Jangipur"/>
+      <Hello name="Ashamin"  age={25} city="Jangipur" hobbies={hobbies}/>
     </div>
   )
 }
--- props/src/components/Hello.jsx
+++ props/src/components/Hello.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 
-function Hello({name="Guest", age="20", city="Mumbai"}) {
+function Hello({name="Guest", age="20", city="Mumbai", hobbies}) {
+
   return (
     <div>
       <h1>Hello, {name}</h1>
       <h2>My age is {age}</h2>
       <h3>My City is {city}</h3>
+      <ul>
+        {hobbies.map((item, index)=><li key={index}>{item}</li>)}
+      </ul>
     </div>
   )
 }
